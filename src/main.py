e40ea1cb532314536e2b1a37e21c8b4327a61de1
Gabriel Freitas <gabriel.freitas@toradex.com>
2024-04-08 18:21:03 -0300

    Try to convert the client to server to connect with the telemetry
    Signed-off-by: Gabriel Freitas <gabriel.freitas@toradex.com>

--- src/main.py
+++ src/main.py
@@ -6,8 +6,11 @@ import sys
 import asyncio
 import argparse  # Import the argparse module
 import threading
+import asyncio, websockets, json
 
 test_mode = False
+
+connected_clients = set()
 
 # Function to parse command-line arguments
 def parse_arguments():
@@ -22,10 +25,67 @@ def parse_arguments():
     parser.add_argument("--delaytime" , type = float, default =  8000         , help = "--|v1|v2|v3|v4|v5|--")
     return parser.parse_args()
 
+class Signal:
+    def __init__(self):
+        self.__subscribers = []
+
+    def connect(self, slot):
+        self.__subscribers.append(slot)
+
+    def emit(self, *args, **kwargs):
+        for subscriber in self.__subscribers:
+            subscriber(*args, **kwargs)
+
+my_signal = Signal()
+
+def my_slot_function(message, additional_info=None):
+    print(f"Signal received with message: {message}")
+
+
+async def send_control_instructions():
+    print("Sending control instructions to any connected robot...")
+    control_message = {
+        "command": "back",
+        "args": {}
+    }
+    for client in connected_clients:
+        asyncio.create_task(client.send(json.dumps(control_message)))
+
+async def handler(websocket, path):
+    global connected_clients
+    client_ip, client_port = websocket.remote_address
+    print(f"New client connected: {client_ip}:{client_port}")
+
+    # Connect the slot to the signal   
+    connected_clients.add(websocket)
+    try:
+        while True:
+            message = await websocket.recv()
+            my_signal.emit(message)
+            #print(f"Received telemetry message from {client_ip}: {message}")
+    finally:
+        print(f"Client disconnected: {client_ip}:{client_port}")
+        connected_clients.remove(websocket)
+
+async def start_server(ip, port):
+    async with websockets.serve(handler, ip, port):
+        print(f"Server started on {ip}:{port}")
+        await asyncio.Future()  # Run forever
+
+async def send_command():
+    while True:
+        await asyncio.sleep(7)
+        await send_control_instructions() 
+
 async def main():
     args = parse_arguments()
     address = args.address.split(":")
     ip, port = address[0], int(address[1])
+
+    await asyncio.gather(
+        start_server(ip, port),
+        send_command(),
+    )
 
     app = QApplication(sys.argv)
     face_widget = FaceWidget(args.v1, args.v2, args.v3, args.v4, args.v5, args.stepsIn10s, args.delaytime)
@@ -37,7 +97,7 @@ async def main():
 
     client = face_widget
     client.startConnection(f"ws://{ip}:{port}")
-
+    my_signal.connect(my_slot_function)
     # Example sending a message
     client.send_message("Hello from QT Robot!")
         
